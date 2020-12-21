b809455b075c4bc650e3677515d6514247219728
Yevgen <yevgen.yelik@gmail.com>
2020-12-21 17:43:07 +0200

    21.12 fixes

--- foxtr.py
+++ foxtr.py
@@ -1,6 +1,4 @@
-import itertools
 import json
-import os
 
 import requests
 from bs4 import BeautifulSoup
@@ -10,54 +8,6 @@ HEADERS = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15'
 }
-
-
-def get_html(url):
-    html = requests.get(url, headers=HEADERS)
-    return html
-
-
-def get_categories_list(html):
-    soup = BeautifulSoup(html.text, 'html.parser')
-    items = soup.find_all('li', class_='catalog__category-item jslink')[5:8]
-    lst = list(map(lambda item: URL + item.find('div', class_='catalog-sub__body-row').find('a').get('href'), items))
-
-    # for item in items:
-    #     lst.append(URL + item.find('div', class_='catalog-sub__body-row').find('a').get('href'))
-    return lst
-
-
-def get_items_list(html):
-    soup = BeautifulSoup(html.text, 'html.parser')
-    items = soup.find_all('div', class_='card')[:3]
-    lst = list(map(lambda item: URL + item.find('div', class_='card__image').find('a').get('href'), items))
-    #
-    # for item in items:
-    #     lst.append(URL + item.find('div', class_='card__image').find('a').get('href'))
-    return lst
-
-
-def uniq_name():
-    basename = itertools.count()
-    actual_name = f'{basename}.json'
-    while os.path.exists(actual_name):
-        actual_name = f'{next(basename)}.json'
-    return actual_name
-
-
-def fetchReviews(html):
-    soup = BeautifulSoup(html.text, 'html.parser')
-    items = soup.find_all('div', class_='product-comment__item')
-    lst = list(map(lambda item: {
-        'author': item.find('div', class_='product-comment__item-title').get_text(strip=True),
-        'date': item.find('div', class_='product-comment__item-date').get_text(strip=True),
-        'text': item.find('div', class_='product-comment__item-text').get_text(strip=True)
-    }, items))
-
-    with open(f'{uniq_name()}', 'w') as file:
-        json.dump(lst, file, indent=2)
-
-    return lst
 
 
 def parse():
@@ -70,7 +20,44 @@ def parse():
 
         for url in itemUrls:
             itemHtml = get_html(url)
-            print(fetchReviews(itemHtml))
+            fetchReviews(itemHtml)
+
+
+def get_html(url):
+    html = requests.get(url, headers=HEADERS)
+    return html
+
+
+def get_categories_list(html):
+    soup = BeautifulSoup(html.text, 'html.parser')
+    items = soup.find_all('li', class_='catalog__category-item jslink')[5:8]
+    lst = list(map(lambda item: URL + item.find('div', class_='catalog-sub__body-row').find('a').get('href'), items))
+
+    return lst
+
+
+def get_items_list(html):
+    soup = BeautifulSoup(html.text, 'html.parser')
+    items = soup.find_all('div', class_='card')[:3]
+    categ_urls_list = list(map(lambda item: URL + item.find('div', class_='card__image').find('a').get('href'), items))
+
+    return categ_urls_list
+
+
+def fetchReviews(html):
+    soup = BeautifulSoup(html.text, 'html.parser')
+    items = soup.find_all('div', class_='product-comment__item')
+    item_title = soup.find('h1', class_='page__title').get_text(strip=True)
+    lst = list(map(lambda item: {
+        'author': item.find('div', class_='product-comment__item-title').get_text(strip=True),
+        'date': item.find('div', class_='product-comment__item-date').get_text(strip=True),
+        'text': item.find('div', class_='product-comment__item-text').get_text(strip=True)
+    }, items))
+
+    with open(f'{item_title[:30]}.json', 'w') as file:
+        json.dump(lst, file, indent=2)
+
+    return lst
 
 
 parse()
